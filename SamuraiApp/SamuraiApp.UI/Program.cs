92f4cec70fc3a9fdad22d3924929cd09424804dc
msg4edson <msg.edson@gmail.com>
2022-01-20 18:26:28 -0300

    interaction completed

--- SamuraiApp/SamuraiApp.UI/Program.cs
+++ SamuraiApp/SamuraiApp.UI/Program.cs
@@ -1,4 +1,5 @@
-﻿using SamuraiApp.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
 using System.Linq;
@@ -13,29 +14,58 @@ namespace SamuraiApp.UI
         static void Main(string[] args)
         {
             _context.Database.EnsureCreated();
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+            //GetSamurais("Before Add:");
+            //AddSamurai();
+            AddSamurais("Julie", "Sampson");
+            GetSamurais();
+            //GetSamurais("After Add:");
+            //GetSamurais("Show: ");
             Console.Write("Press any key...");
             Console.ReadKey();
         }
 
-        private static void AddSamurai()
+        
+        //Add one at time
+        //private static void AddSamurai()
+        //{
+        //    var samurai = new Samurai { Name = "Edson" };
+        //    _context.Samurais.Add(samurai);
+        //    _context.SaveChanges();
+        //}
+
+        private static void AddSamurais(params string[] names)
         {
-            var samurai = new Samurai { Name = "Edson" };
-            _context.Samurais.Add(samurai);
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
             _context.SaveChanges();
         }
 
-        private static void GetSamurais(string text)
+        //One way to get all values
+        //private static void GetSamurais(string text)
+        //{
+        //    var samurais = _context.Samurais.ToList();
+        //    Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+        //    foreach(var samurai in samurais)
+        //    {
+        //        Console.WriteLine(samurai.Name);
+        //    }
+
+        //}
+
+        private static void GetSamurais()
         {
-            var samurais = _context.Samurais.ToList();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-            foreach(var samurai in samurais)
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurais method")
+                .ToList();
+            Console.WriteLine($"Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
             {
                 Console.WriteLine(samurai.Name);
             }
-            
         }
+
+
     }
 }
